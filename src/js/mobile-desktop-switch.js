63fe2afbc4b38ea39016a3906ebc0001cd088fad
Orion-Technologies <abel.archila@gmail.com>
2025-02-20 11:59:57 -0600

    update img landing

---
+++ src/js/mobile-desktop-switch.js
@@ -0,0 +1,8 @@
+function getWindowSize() {
+   const width  = window.innerWidth;
+   const height = window.innerHeight;
+
+   console.log(`Window size: ${width} x ${height} `);
+}
+
+getWindowSize();
