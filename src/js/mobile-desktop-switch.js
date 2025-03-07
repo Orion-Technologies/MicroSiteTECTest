469a51eea19fd8a5266655686a21cb9f2c3a9724
Orion-Technologies <abel.archila@gmail.com>
2025-03-07 13:49:19 -0600

    se arreglaron las indentaciones

--- src/js/mobile-desktop-switch.js
+++
@@ -1,8 +0,0 @@
-function getWindowSize() {
-   const width  = window.innerWidth;
-   const height = window.innerHeight;
-
-   console.log(`Window size: ${width} x ${height} `);
-}
-
-getWindowSize();
